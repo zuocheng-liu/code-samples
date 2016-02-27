d59f870f660534d2b6b8d3bd6b33cf74a4d42faa
Zuocheng Liu <zuocheng.liu@gmail.com>
2016-02-28 07:46:44 +0800

    add sort algorithm

--- algorithm/sort/sort.cpp
+++ algorithm/sort/sort.cpp
@@ -1,9 +1,108 @@
+/**
+ * Parameter check
+ * The loop termination conditions
+ *
+ */
+
+
 #include <iostream>
 using namespace std;
 
 int unorder[12] = {7,3,1,3,6,7,8,9,9,5,3,1};
-int ordered[12];
+int ordered[12] = {1,1,3,3,3,5,6,7,7,8,9,9};
+
+/**
+ * Insert Sort
+ * Time complexition : O(N)
+ * Space complexition : O(1)
+ */
+int insert_sort(int* list, int listLength) {
+    int start, tmp, pre;
+    if (NULL == list || listLength < 0) {
+        return -1;
+    }
+    for (start = 1; start < listLength; ++start) {
+        if (list[start] < list[start - 1]) {
+            tmp = list[start];
+            for (pre = start - 1; list[pre] > tmp; --pre ) {
+                list[pre + 1] = list[pre]; 
+            }
+            list[pre] = tmp;
+        }
+    }
+}
+
+int shell_sort(int* list, int listLength) {
+    
+}
+int select_sort(int* list, int listLength) {
+    int start;
+    int i;
+    int min;
+    for (start = 0; start < listLength - 1; ++start) {
+        for (i = start,min = i; i < listLenght - 1; ++i) {
+           if (list[min] > list[i]) {
+               min = i;
+           } 
+        }
+        list[start] ^= list[min] ^= list[start] ^= list[min];
+    }
+}
+
+int bubble_sort(int* list, int listLength) {
+    int i,j;
+    for (i = 0; i < listLength - 1, ++i) {
+        for (j = 0; j < listLength - i - 2; ++j) {
+            if (list[j] > list[j + 1]) {
+                list[j + 1] ^= list[j] ^= list[j + 1] ^= list[j];
+            }
+        }
+    }
+}
+
+int quick_sort(int* list, int listLength) {
+    int m,s,t;
+    if (NULL == list || listLength < 0) {
+        return -1;
+    }
+    if (listLenght <= 1) {
+        return 0;
+    }
+    s = 0;
+    t = listLength - 1;
+    m = list[s];
+    while (s < t) {
+        if (list[t] < m) {
+            list[s] = list[t];
+            ++s ;
+        } else {
+            --t;
+            continue;
+        }
+        if (list[s] > m) {
+            list[t] = list[s];
+            --t;
+        } else {
+            s++;
+            continue;
+        }
+    }
+    list[t] = m;
+    quick_sort(list, t);
+    quick_sort(list + t, length - t);
+}
+
+int heap_sort(int* list, int length) {
+    int i,j;
+    if (NULL == list || length <= 1) {
+        return 0;
+    }
+    
+    list[length - 1] ^= list[0] ^= list[length - 1] ^= list[0];
+    return heap_sort(list, length - 1);
+}
+
+
 int main() {
-    for (int i ;)
     
 }
